f9ed303e97f8ff54e03d02bba824ab37fa463523
kkh4606 <winningelevendatabackup@gmail.com>
2025-07-23 19:26:46 +0630

    rename and move

--- app/models.py
+++ app/models.py
@@ -8,6 +8,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     email = Column(String, unique=True, index=True, nullable=False)
     password = Column(String, nullable=False, index=True)
+    date_created = Column(DateTime(timezone=True), server_default=func.now())
     todos = relationship("Todo", back_populates="user")
 
 

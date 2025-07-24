5f5c39e5d0fc38515b8e68aaac88c0923c846f1c
kkh4606 <winningelevendatabackup@gmail.com>
2025-07-24 23:23:01 +0630

    add middleware

--- app/main.py
+++ app/main.py
@@ -3,9 +3,23 @@ from . import models
 from .database import Base, engine
 from app.routers import todos, auth, user
 
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # or replace "*" with your React URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 
 
 app.include_router(todos.router)
